03b330a82981dfa3f9a7a8e504ec79525aa61eb1
JuanJo Ciarlante <juanjosec@gmail.com>
2025-03-14 21:50:04 -0300

    adjust .zshrc-jjo.d/plugins.zsh
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- .zshrc-jjo.d/plugins.zsh
+++ .zshrc-jjo.d/plugins.zsh
@@ -1,4 +1,4 @@
 # autosuggestions install:
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 plugins=($plugins zsh-autosuggestions)
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=5'
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=2'
