8b5ef2da9641bca3656a5b4b4ceb4d95aacf00ac
JuanJo Ciarlante <juanjosec@gmail.com>
2025-07-18 11:05:37 -0300

    add/update .zshrc-jjo.d/plugins.zsh
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- .zshrc-jjo.d/plugins.zsh
+++ .zshrc-jjo.d/plugins.zsh
@@ -1,4 +1,6 @@
 # autosuggestions install:
+# Good resource -> https://catalins.tech/zsh-plugins/
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-plugins=($plugins zsh-autosuggestions)
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+plugins=($plugins zsh-autosuggestions zsh-syntax-highlighting)
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=2'
