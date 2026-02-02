cada4ff2ee493c5012890fc88da16908ea183e9e
JuanJo Ciarlante <juanjosec@gmail.com>
2026-02-02 15:51:46 -0300

    updates
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- .zshrc-jjo.d/plugins.zsh
+++ .zshrc-jjo.d/plugins.zsh
@@ -2,5 +2,5 @@
 # Good resource -> https://catalins.tech/zsh-plugins/
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-plugins=($plugins zsh-autosuggestions zsh-syntax-highlighting)
+plugins=($plugins zsh-autosuggestions zsh-syntax-highlighting clipboard)
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=2'
