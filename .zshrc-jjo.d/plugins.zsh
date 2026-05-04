36354ccd660a4ee5c6d290b58485a4fb8b9ed6e6
JuanJo Ciarlante <juanjosec@gmail.com>
2026-05-04 09:42:33 -0300

    .zshrc updates
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- .zshrc-jjo.d/plugins.zsh
+++ .zshrc-jjo.d/plugins.zsh
@@ -2,5 +2,7 @@
 # Good resource -> https://catalins.tech/zsh-plugins/
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-plugins=($plugins zsh-autosuggestions zsh-syntax-highlighting clipboard)
+# git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+# git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+plugins=($plugins zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete clipboard)
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=2'
